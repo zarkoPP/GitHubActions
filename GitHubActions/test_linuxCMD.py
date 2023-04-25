b9e2345f183253942ec88691202a4cf2f8a64808
zarkoPP <83474116+zarkoPP@users.noreply.github.com>
2023-04-25 12:43:29 +0200

    Create test_linuxCMD.py

---
+++ GitHubActions/test_linuxCMD.py
@@ -0,0 +1,15 @@
+# Define the numbers
+num1 = 10
+num2 = 5
+
+# Perform operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Print the results
+print("Addition result:", addition)
+print("Subtraction result:", subtraction)
+print("Multiplication result:", multiplication)
+print("Division result:", division)
